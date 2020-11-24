ec3eeee733848fab3653bb8cd8c3fa5df2c63b4c
emani <emani@SRINI-LAPTOP.attlocal.net>
2020-11-23 19:46:54 -0800

    FirefoxBrowser setup in Webdriver under Maven project 11/23/2020

--- SeleniumLearning_Maven/src/test/java/com/testcases/firefoxbrowser.java
+++ SeleniumLearning_Maven/src/test/java/com/testcases/firefoxbrowser.java
@@ -8,9 +8,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class firefoxbrowser {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//FirefoxBrowser setup in Webdriver under Maven project 11/23/2020
 		WebDriverManager.firefoxdriver().setup();
 		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.eenadu.net/");
+		String title = driver.getTitle();
+		System.out.println(title);
 
 	}
 
