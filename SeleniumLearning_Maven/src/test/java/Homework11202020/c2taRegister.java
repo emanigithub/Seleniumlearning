1f8d534963f4c8f2c3ae70e19acb3ee34543bbb2
emani <emani@SRINI-LAPTOP.attlocal.net>
2020-11-23 20:38:32 -0800

    Registering to c2ta.co.in Site in Webdriver under Maven project 11/23/20

--- SeleniumLearning_Maven/src/test/java/Homework11202020/c2taRegister.java
+++ SeleniumLearning_Maven/src/test/java/Homework11202020/c2taRegister.java
@@ -1,5 +1,6 @@
 package Homework11202020;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,11 +9,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class c2taRegister {
 
 	public static void main(String[] args) {
-		// Registering to https://c2ta.co.in Site
+		// Registering to https://c2ta.co.in Site ..... Do not Register Again
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://c2ta.co.in/register/");
-		
+		driver.findElement(By.id("reg_username")).sendKeys("emani_sree");
+		driver.findElement(By.id("reg_email")).sendKeys("emani_sree@yahoo.com");
+		driver.findElement(By.id("reg_password")).sendKeys("Myc2ta123@");	
+		driver.findElement(By.xpath("//*[@id=\"post-306\"]/div/div/div/div/div/form/p/button")).click();
 
 	}
 
